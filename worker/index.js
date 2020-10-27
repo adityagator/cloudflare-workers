4cd18e317729e0c43d78f30ec7f248c883a3a0a8
Aditya Bharadwaj <adityabharadwaj@Adityas-MBP.attlocal.net>
2020-10-27 15:45:51 -0700

    add route to api

--- worker/index.js
+++ worker/index.js
@@ -1,3 +1,4 @@
+// Create the class for link objects
 class LinkObject {
   constructor(name, url) {
       this.name = name;
@@ -7,14 +8,13 @@ class LinkObject {
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 var linkObjects = new Array(new LinkObject("testName1", "testUrl1"), 
-                            new LinkObject("testName2", "testUrl2"));
+                            new LinkObject("testName2", "testUrl2"),
+                            new LinkObject("testName3", "testUrl3"));
 async function handleRequest(request) {
-  return new Response(JSON.stringify(linkObjects), {
-    headers: { 'content-type': 'application/json' },
+  if (request.url.includes("/links"))
+    return new Response(JSON.stringify(linkObjects), {
+      headers: { 'content-type': 'application/json' },
   })
 }
